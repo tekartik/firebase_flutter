6026f0de2cdab14a5ae3bdae7b7edffbcc1dd249
Alexandre Roux <alex@tekartik.com>
2023-07-22 14:16:15 +0200

    test: timestamp

---
+++ firestore_flutter/test/firestore_native_test.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('timestamp', () {
+    test('epoch', () {
+      var timestamp = Timestamp(1, 123000000);
+      expect(
+          Timestamp.fromMillisecondsSinceEpoch(
+              timestamp.millisecondsSinceEpoch),
+          timestamp);
+      timestamp = Timestamp(1, 123456000);
+      expect(
+          Timestamp.fromMicrosecondsSinceEpoch(
+              timestamp.microsecondsSinceEpoch),
+          timestamp);
+    });
+  });
+}
