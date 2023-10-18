b2f92662910a5085fe6c316bc047057a40b01aa8
Alexandre Roux <alex@tekartik.com>
2023-10-18 10:23:28 +0200

    deprecate firestoreService

--- firestore_flutter/test/firestore_flutter_test.dart
+++ firestore_flutter/test/firestore_flutter_test.dart
@@ -6,7 +6,6 @@ import 'package:tekartik_firebase_firestore_flutter/firestore_flutter.dart';
 void main() {
   group('firestore_flutter', () {
     test('api', () {
-      firestoreService;
       firestoreServiceFlutter;
     });
   });
